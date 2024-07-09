a313e0a2b3ed41a8bf7a3cca2e3274a8cc585503
Mohamed Ahmed <ma07041705@gmail.com>
2024-07-09 11:52:45 +0300

    colum

--- server/models.py
+++ server/models.py
@@ -14,6 +14,7 @@ class Restaurant(db.Model, SerializerMixin):
     __tablename__ = 'restaurant'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
+    image = db.Column(db.String(100), nullable=False)
     location = db.Column(db.String(200), nullable=False)
 
     reservations = db.relationship('Reservation', back_populates='restaurant', lazy=True)
