89cf9ae55b245536e94c8834b1572a01a9fa0949
Mohamed Ahmed <ma07041705@gmail.com>
2024-07-09 11:35:53 +0300

    implemented relationship

---
+++ server/app.py
@@ -0,0 +1,30 @@
+# Standard library imports
+
+# Remote library imports
+from flask import Flask
+from flask_cors import CORS
+from flask_migrate import Migrate
+from flask_restful import Api
+from models import db, Restaurant,Customer ,Reservation, ReservationTable,Table
+
+
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dine_mate.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+
+migrate = Migrate(app, db)
+db.init_app(app)
+
+api = Api(app)
+
+CORS(app)
+
+
+
+
+if __name__ == "__main__":
+    app.run(port=5555, debug=True)
