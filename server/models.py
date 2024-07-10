2488f4fdc2cdb5ce8cf00e1e49f36bce4e53e179
Mohamed Ahmed <ma07041705@gmail.com>
2024-07-10 15:03:48 +0300

    tables

--- server/models.py
+++ server/models.py
@@ -33,18 +33,6 @@ class Customer(db.Model, SerializerMixin):
 
 
 
-class Table(db.Model, SerializerMixin):
-    __tablename__ = 'table'
-    id = db.Column(db.Integer, primary_key=True)
-    table_number = db.Column(db.Integer, nullable=False)
-    capacity = db.Column(db.Integer, nullable=False)
-
-    reservations = db.relationship('ReservationTable', back_populates='table', lazy=True)
-    serialize_rules = ('-reservations',)
-
-
- 
-
 class Reservation(db.Model, SerializerMixin):
     __tablename__ = 'reservation'
     id = db.Column(db.Integer, primary_key=True)
@@ -52,14 +40,11 @@ class Reservation(db.Model, SerializerMixin):
     number_guests = db.Column(db.Integer,  nullable=False)
     customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'), nullable=False)
     restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'), nullable=False)
-    table_id = db.Column(db.Integer, db.ForeignKey('table.id'), nullable=False)
 
     restaurant = db.relationship('Restaurant', back_populates='reservations')
     customer = db.relationship('Customer', back_populates='reservations')
-    tables = db.relationship('ReservationTable', back_populates='reservation', lazy=True)
-    serialize_rules = ('-restaurant.reservations', '-customer.reservations', '-tables.reservation')
+    serialize_rules = ('-restaurant.reservations', '-customer.reservations',)
    
-
 
 
 
@@ -67,8 +52,6 @@ class ReservationTable(db.Model, SerializerMixin):
     __tablename__ = 'reservation_table'
     id = db.Column(db.Integer, primary_key=True)
     reservation_id = db.Column(db.Integer, db.ForeignKey('reservation.id'), nullable=False)
-    table_id = db.Column(db.Integer, db.ForeignKey('table.id'), nullable=False)
 
-    reservation = db.relationship('Reservation', back_populates='tables')
-    table = db.relationship('Table', back_populates='reservations')
-    serialize_rules = ('-reservation.tables', '-table.reservations')
+
+    serialize_rules = ('-reservation',)
