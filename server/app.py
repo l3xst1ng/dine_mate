4f01603f2ca70136d6ff51568fb531cab627e0a3
GithinjiKelvin <githinjikelvin59@gmail.com>
2024-07-10 09:37:37 +0300

    Adding restaurant & table routes

--- server/app.py
+++ server/app.py
@@ -1,10 +1,9 @@
 # Standard library imports
 
 # Remote library imports
-from flask import Flask
+from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
-from flask_restful import Api
 from models import db, Restaurant,Customer ,Reservation, ReservationTable,Table
 
 
@@ -19,11 +18,57 @@ app.json.compact = False
 migrate = Migrate(app, db)
 db.init_app(app)
 
-api = Api(app)
-
 CORS(app)
 
-
+@app.route('/tables', methods=['GET', 'POST'])
+def tables():
+    if request.method == 'GET':
+        tables = []
+        for table in Table.query.all():
+            table_dict = table.to_dict(rules=('-reservations',))
+            tables.append(table_dict)
+        response = make_response(
+                tables, 200
+            )
+        return response
+    
+    elif request.method=='POST':
+        new_table = Table(
+            table_number = request.form.get("table_number"),
+            capacity=request.form.get("capacity"),
+        )
+        db.session.add(new_table)
+        db.session.commit()
+        table_dict=new_table.to_dict()
+        response=make_response(
+            table_dict, 201
+        )
+        return response
+    
+@app.route('/restaurants', methods=['GET', 'POST'])
+def restaurants():
+    if request.method == 'GET':
+        restaurants = []
+        for restaurant in Restaurant.query.all():
+            restaurant_dict = restaurant.to_dict(rules=('-reservations',))
+            restaurants.append(restaurant_dict)
+        response = make_response(
+                restaurants, 200
+            )
+        return response
+    
+    elif request.method=='POST':
+        new_restaurant = Restaurant(
+            name = request.form.get("name"),
+            location = request.form.get("location"),
+        )
+        db.session.add(new_restaurant)
+        db.session.commit()
+        restaurant_dict=new_restaurant.to_dict()
+        response=make_response(
+            restaurant_dict, 201
+        )
+        return response
 
 
 if __name__ == "__main__":
