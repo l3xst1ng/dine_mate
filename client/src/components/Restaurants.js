984c0a4243eb690b04ce51970d3832409198183e
l3xst1ng <howdy.martt@gmail.com>
2024-07-12 22:38:14 +0300

    Refactor Restaurants component and update image handling

--- client/src/components/Restaurants.js
+++ client/src/components/Restaurants.js
@@ -1,28 +1,54 @@
-import React, { useState } from 'react';
+
+import React, { useState, useEffect } from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import './Restaurants.css';
 import { initialRestaurants } from '../data/restaurantData';
 
+// Import all images
+import serenityLogo from '../assets/images/Serenity.png';
+import shawrysLogo from '../assets/images/Shawrieskitchen.png';
+import bigBiteLogo from '../assets/images/bigbyte.png';
+// Placeholder image
+import coverImage from '../assets/images/Cover.png';
+
+// object to map image filenames to their imported versions
+const imageMap = {
+  'Serenity.png': serenityLogo,
+  'Shawries.png': shawrysLogo,
+  'bigbyte.png': bigBiteLogo,
+  
+};
+
 const Restaurants = () => {
   const [restaurants, setRestaurants] = useState(initialRestaurants);
+  // const [isLoading, setIsLoading] = useState(true);
+
+  // useEffect(() => {
+  //   // Simulate loading delay
+  //   const timer = setTimeout(() => setIsLoading(false), 1000);
+  //   return () => clearTimeout(timer);
+  // }, []);
 
   const validationSchema = Yup.object().shape({
     name: Yup.string().required('Restaurant name is required'),
     location: Yup.string().required('Location is required'),
     description: Yup.string().required('Description is required'),
-    image: Yup.string().url('Must be a valid URL').required('Image URL is required'),
+    image: Yup.string().required('Image is required'),
   });
 
   const handleAddRestaurant = (values, { resetForm }) => {
     const newRestaurant = {
       id: restaurants.length + 1,
       ...values,
-      // Note To Team: description and image are included here but won't be stored in our database. We can do way with also.
     };
     setRestaurants([...restaurants, newRestaurant]);
     resetForm();
   };
+
+  // if (isLoading) {
+  //   return <div className="loading">Loading...</div>;
+  // }
 
   return (
     <div className="restaurant-container">
@@ -30,10 +56,9 @@ const Restaurants = () => {
         {restaurants.map((restaurant) => (
           <div key={restaurant.id} className="restaurant-card">
             <img 
-              src={restaurant.image} 
+              src={imageMap[restaurant.image] || coverImage} 
               alt={restaurant.name} 
               className="restaurant-logo"
-              onError={(e) => {e.target.onerror = null; e.target.src = '../assets/images/Cover.png'}}
             />
             <div className="restaurant-info">
               <h3 className="restaurant-name">{restaurant.name}</h3>
@@ -44,7 +69,6 @@ const Restaurants = () => {
         ))}
       </div>
 
-{/* Restaurant Form Field */}
       <Formik
         initialValues={{ name: '', location: '', description: '', image: '' }}
         validationSchema={validationSchema}
@@ -61,7 +85,7 @@ const Restaurants = () => {
             <Field name="description" placeholder="Description" className="form-input" />
             <ErrorMessage name="description" component="div" className="error-message" />
 
-            <Field name="image" placeholder="Image URL" className="form-input" />
+            <Field name="image" placeholder="Image filename" className="form-input" />
             <ErrorMessage name="image" component="div" className="error-message" />
 
             <button type="submit" className="add-button">Add Restaurant</button>
